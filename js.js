b6d249dd1ec8faac13964962092b58589e3e1674
mykyta stoliarov <mykyta.stoliarov@gmail.com>
2022-10-21 10:33:01 -0400

    added a basic functionality, now you can click on a class picture and see all cards for a specific class"
    "

--- js.js
+++ js.js
@@ -1,13 +1,34 @@
 //searchInput.value = "ASDASD";
 "use strict";
+
 let data = [];
 window.onload = async function () {
   const options = {
     method: "GET",
+    url: "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/classic",
     headers: {
-      "X-RapidAPI-Key": "9e51f5dc49mshdf69009e5ae6b29p157366jsn34eb8c0dd070",
-      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+      "x-rapidapi-key": "59489fd7femshd11da986701b4cep1b9b50jsn3ce72df24df4",
+      "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
     },
+  };
+  let rawDataClasses = [];
+  await fetch(options.url, options)
+    .then((res) => res.json())
+    .then((json) => (rawDataClasses = json))
+    .catch((err) => console.error("error:" + err));
+
+  const filterCardsbyClass = (className, cards) => {
+    const newSection = document.createElement("section");
+    newSection.class = "allCardsByClass";
+    document.body.appendChild(newSection);
+    cards.forEach((element) => {
+      if (element.playerClass == className && element.img) {
+        console.log("I am here");
+        const avatar = document.createElement("img");
+        avatar.src = element?.img;
+        newSection.appendChild(avatar);
+      }
+    });
   };
 
   data = await fetch(
@@ -20,34 +41,25 @@ window.onload = async function () {
 
   //assigning array of classes to data
   data = data.classes;
-  //**********************************                                       Declaration
+  //**********************************           Declaration          **********************************
   const inputField = document.getElementById("search1");
   const picSection = document.getElementById("results");
-  //const avatar = document.createElement("img");
-
+  //   const avatar = document.createElement("img");
   const dontInclude = ["dream"];
+  //**********************************          finish
   const searchInput = () => {
     //clear section
     picSection.innerHTML = "";
 
     //comparing data for match
-      let result =  formatArray(
-        data.filter((element) =>
-          element.toLowerCase().includes(inputField.value.toLowerCase())
-        )
-      );
-    
-    console.log(result);
+    let result = formatArray(
+      data.filter((element) =>
+        element.toLowerCase().includes(inputField.value.toLowerCase())
+      )
+    );
 
     if (result != 0) {
-      result.forEach((hero) => {
-        if (!dontInclude.includes(hero)) {
-          const avatar = document.createElement("img");
-          avatar.src = `pics/${hero}.png`;
-          avatar.height = "300";
-          picSection.appendChild(avatar);
-        }
-      });
+      updateUI(result);
     }
   };
 
@@ -58,5 +70,25 @@ window.onload = async function () {
     });
     return solution;
   };
+  updateUI(data);
+  //********************************         SEARCH TRIGGER
   inputField.addEventListener("keyup", searchInput);
+
+  function updateUI(data) {
+    formatArray(data).forEach((hero) => {
+      console.log(hero);
+      if (!dontInclude.includes(hero)) {
+        const avatar = document.createElement("img");
+
+        avatar.src = `pics/${hero}.png`;
+        avatar.height = "300";
+        avatar.id = hero;   
+        picSection.appendChild(avatar);
+        avatar.addEventListener("click", () => {
+          console.log(avatar.id);
+          filterCardsbyClass("Druid", rawDataClasses);
+        });
+      }
+    });
+  }
 };
